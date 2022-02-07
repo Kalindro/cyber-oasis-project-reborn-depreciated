250c75e91511f1c064aedcf38f6567103750e81f
mrkalindro@gmail.com <8Bmtm&*2BdInBRb>
2022-02-07 15:11:30 +0100

    Boss

--- Strategies/AB_Long/Binance_AB_L/AB_L_Bot_Binance_Backtest.py
+++ Strategies/AB_Long/Binance_AB_L/AB_L_Bot_Binance_Backtest.py
@@ -1,6 +1,6 @@
 from Gieldy.Binance.API_initiation_Binance import API_initiation
 
-from Strategies.AB_Long_Bot.Backtest_AB_L import backtest_AB_l
+from Strategies.AB_Long.Backtest_AB_L import backtest_AB_l
 
 if __name__ == "__main__":
     API = API_initiation()
