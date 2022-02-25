e5e55bd7940c519a858386b3c783493c6a462834
mrkalindro@gmail.com <8Bmtm&*2BdInBRb>
2022-02-25 20:59:57 +0100

    Boss

--- Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
+++ Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
@@ -3,18 +3,37 @@ from pathlib import Path
 import asyncio
 import os
 
+from Gieldy.Refractor_general.Arb_helpers import load_position_table
+
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-current_path = os.path.dirname(os.path.abspath(__file__))
-project_path = Path(current_path).parent.parent
-os.environ["ANCHOR_WALLET"] = f"{project_path}\Gieldy\APIs\Solana_Drift_Bina_ARB.json"
+
+class DriftBinaARBLive:
+
+    def __init__(self):
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        project_path = Path(current_path).parent.parent
+        os.environ["ANCHOR_WALLET"] = f"{project_path}\Gieldy\APIs\Solana_Drift_Bina_ARB.json"
+        self.QUOTE_PRECISION = 1e6
+
+    async def accinit(self):
+        user_account = dict()
+        user_account["drift_acct"] = await ClearingHouse.create_from_env("mainnet")
+        user_account["drift_user"] = ClearingHouseUser(user_account["drift_acct"], user_account["drift_acct"].program.provider.wallet.public_key)
+        user_account["drift_user_acct"] = await user_account["drift_user"].get_user_account()
+        return user_account
+
+    async def get_margin_account_info(self):
+        account = await self.accinit()
+        margin_info = dict()
+        margin_info['total_collateral'] = await account["drift_user"].get_total_collateral()/self.QUOTE_PRECISION
+        margin_info['unrealised_pnl'] = await account["drift_user"].get_unrealised_pnl(0)/self.QUOTE_PRECISION
+        margin_info['leverage'] = await account["drift_user"].get_leverage()
+        margin_info['free_collateral'] = await account["drift_user"].get_free_collateral()/self.QUOTE_PRECISION
+        print(margin_info)
+        return margin_info
 
 
-async def accinit():
-    drift_acct = await ClearingHouse.create_from_env("mainnet")
-    await drift_acct.initialize_user_account_and_deposit_collateral(amount=3000000, collateral_account_public_key=drift_acct.program.provider.wallet.public_key)
-    # drift_user = ClearingHouseUser(drift_acct, "FHL6i18MNUoLhsMqwUFQfQLyVQy4R1jhzxG95NeKBkec")
-    # drift_user_acct = await drift_user.get_user_account()
-    # print(drift_user_acct)
-asyncio.run(accinit())
+asyncio.run(DriftBinaARBLive().get_margin_account_info())
+
