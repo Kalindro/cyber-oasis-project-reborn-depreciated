0ddefdaaee497877ad38ba493b8ee782bd488c80
mrkalindro@gmail.com <8Bmtm&*2BdInBRb>
2022-04-26 17:25:31 +0200

    Boss

--- Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
+++ Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
@@ -56,6 +56,7 @@ class Initialize:
                 source = f"{project_path}/History_data/Drift/5S/Price_gaps_5S_LIVE_WRITE.csv"
                 destination = f"{project_path}/History_data/Drift/5S/Price_gaps_5S_COPY_READ.csv"
                 shutil.copyfile(source, destination)
+                time.sleep(0.5)
                 historical_arb_df = pd.read_csv(f"{project_path}/History_data/Drift/5S/Price_gaps_5S_COPY_READ.csv", index_col=0, parse_dates=True, low_memory=False)
                 if (len(historical_arb_df) < 2) or np.isnan(historical_arb_df.iloc[-1]["bina_price"]) or np.isnan(historical_arb_df.iloc[-1]["drift_price"]) or np.isnan(historical_arb_df.iloc[-1]["gap_perc"]):
                     x = 5/0  # Exception force
