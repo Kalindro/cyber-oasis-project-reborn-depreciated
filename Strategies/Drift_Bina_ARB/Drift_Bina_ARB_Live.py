15cd15f47964c54e0d4c2a334d30aefe61f36190
mrkalindro@gmail.com <8Bmtm&*2BdInBRb>
2022-05-01 12:17:38 +0200

    Boss

--- Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
+++ Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
@@ -558,7 +558,7 @@ class LogicHandle(Initialize):
                     pass
                     print(f"{round_time(dt=dt.datetime.now(), date_delta=dt.timedelta(seconds=5))} --- Logic loop %s seconds ---" % (round(time.perf_counter() - logic_start_time, 2)))
 
-                if x > 50:
+                if x > 25:
                     balances_dict = await self.get_balances_summary(API_drift, API_binance)
                     API_drift = await self.initiate_drift()
                     API_binance = self.initiate_binance()
