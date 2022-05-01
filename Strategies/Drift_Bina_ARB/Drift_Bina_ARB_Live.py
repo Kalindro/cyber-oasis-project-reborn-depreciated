6b9ae609b857b1a463d61866ea7976dbf9b7ec49
mrkalindro@gmail.com <8Bmtm&*2BdInBRb>
2022-05-01 12:21:02 +0200

    Boss

--- Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
+++ Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
@@ -42,7 +42,7 @@ class Initialize:
         self.FAST_AVG = 18
         self.QUARTILE = 0.15
         self.MIN_REGULAR_GAP = 0.44
-        self.MIN_CLOSING_GAP = 0.00
+        self.MIN_CLOSING_GAP = 0.02
         self.LEVERAGE = 4
         self.COINS_AT_ONCE = 5
         self.DRIFT_BIG_N = 1_000_000
