060c91126a62c38ee30996b0e91d6ea392c23419
mrkalindro@gmail.com <8Bmtm&*2BdInBRb>
2022-05-06 01:50:50 +0200

    Boss

--- Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
+++ Strategies/Drift_Bina_ARB/Drift_Bina_ARB_Live.py
@@ -560,9 +560,8 @@ class LogicHandle(Initialize):
                 expected = 5
                 if elapsed < expected:
                     time.sleep(expected - elapsed)
-                elif elapsed > 20:
-                    pass
-                print(f"{round_time(dt=dt.datetime.now(), date_delta=dt.timedelta(seconds=5))} --- Logic loop %s seconds ---" % (round(time.perf_counter() - logic_start_time, 2)))
+                elif elapsed > 10:
+                    print(f"{round_time(dt=dt.datetime.now(), date_delta=dt.timedelta(seconds=5))} --- Logic loop %s seconds ---" % (round(time.perf_counter() - logic_start_time, 2)))
 
                 if loop_counter_logic > 25:
                     balances_dict = await self.get_balances_summary(API_drift, API_binance)
