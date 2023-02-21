a7fb439e05de3ff7999fcfefeb30637d856704b0
Kalindro <25466220+Kalindro@users.noreply.github.com>
2023-02-21 17:02:21 +0100

    Boss

--- chatGPT/main.py
+++ chatGPT/main.py
@@ -1,31 +1,40 @@
 from API.chatGPT_initiator import initiate_chatGPT_wrapper, initiate_chatGPT_API
+from dataclasses import dataclass
 
 
+@dataclass
 class ChatGPTDialog:
+    API_mode = 1
 
-    def __init__(self):
-        self.openai = initiate_chatGPT_API()
-        self.chatbot = initiate_chatGPT_wrapper()
-
-    def ask_question_wrapper(self, question):
-        prev_text = ""
+    def ask_question_wrapper(self, question: str) -> str:
+        chatbot = initiate_chatGPT_wrapper()
         print("Asked question")
         print("chatGPT:")
-        for data in self.chatbot.ask(question):
-            message = data["message"][len(prev_text):]
-            print(message, end="", flush=True)
-            prev_text = data["message"]
-        print()
+        print(chatbot.ask(question))
 
-    def ask_question_API(self, question):
+        for data in chatbot.ask(question):
+            print(data)
+            response = data["message"]
+
+        return response
+
+    def ask_question_API(self, question: str) -> str:
+        openai = initiate_chatGPT_API()
         print("Asked question")
         print("chatGPT:")
-        response = self.openai.Completion.create(model="text-davinci-003", prompt=question, temperature=0,
-                                                 max_tokens=60, top_p=1.0, frequency_penalty=0.5, presence_penalty=0.0)
-        print(response["choices"][0]["text"])
+        response = openai.Completion.create(model="text-davinci-003", prompt=question, temperature=0, max_tokens=60,
+                                            top_p=1.0, frequency_penalty=0.5, presence_penalty=0.0)
+
+        return response["choices"][0]["text"].lstrip()
+
+    def main(self, question: str) -> str:
+        response = self.ask_question_API(question) if self.API_mode == 1 else self.ask_question_wrapper(question)
+
+        print(response)
+        return response
 
 
 if __name__ == "__main__":
-    tweet = "Sram sobie"
-    full_question = f"Decide whether a Tweet's sentiment is positive, neutral, or negative and how impactful. /Tweet: {tweet}"
-    ChatGPTDialog().ask_question_API(question=full_question)
+    tweet = "Visitors Can Sell Bitcoin in Dubai for Cash in 2023 at SBID Crypto OTC - NFC"
+    full_question = f"Decide if this is positive, neutral or negative news. Tweet: {tweet}"
+    ChatGPTDialog().main(question=full_question)
--- webscrapper/web_scrapper.py
+++ webscrapper/web_scrapper.py
@@ -1,22 +1,52 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pandas as pd
+from fake_useragent import UserAgent
+from dataclasses import dataclass
+from pandas import DataFrame as df
 
 
-def tree_of_alpha_scrapper():
-    url = "https://news.treeofalpha.com/"
+@dataclass
+class CryptoNewsScrapper:
+    SITE_URL: str = "https://news.treeofalpha.com/"
 
-    driver = webdriver.Chrome()
+    def main(self):
+        soup = self.get_soup(site_url=self.SITE_URL)
+        articles_dataframe = self.tree_of_alpha_scrapper(soup=soup)
 
-    driver.get(url)
-    driver.implicitly_wait(100)
-    html = driver.page_source
-    driver.quit()
+        return articles_dataframe
 
-    soup = BeautifulSoup(html, "html.parser")
+    @staticmethod
+    def tree_of_alpha_scrapper(soup):
+        articles = soup.find_all("div", class_="contentWrapper")
 
-    articles = soup.find_all("div", class_="contentWrapper")
+        articles_dataframe = df()
+        for article in articles:
+            message = article.find("h2", class_="contentTitle").text.strip()
+            timestamp = article.find("div", class_="originTime").text.strip()
+            article_data = {"timestamp": [timestamp], "message": [message]}
+            article_df = df(article_data)
+            articles_dataframe = pd.concat([articles_dataframe, article_df], ignore_index=True)
 
-    for article in articles:
-        message = article.find("h2", class_="contentTitle").text.strip()
-        timestamp = article.find("div", class_="originTime").text.strip()
-        print(f"{timestamp}: {message}")
+        return articles_dataframe
+
+    @staticmethod
+    def get_soup(site_url: str) -> BeautifulSoup:
+        user_agent = UserAgent().random
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument(f'user-agent={user_agent}')
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+
+        driver.get(site_url)
+        driver.implicitly_wait(100)
+        html = driver.page_source
+        driver.quit()
+
+        soup = BeautifulSoup(html, "html.parser")
+
+        return soup
+
+
+if __name__ == "__main__":
+    CryptoNewsScrapper().main()
