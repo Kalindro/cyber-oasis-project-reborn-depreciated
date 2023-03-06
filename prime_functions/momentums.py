ed379d845452878f26f8b020b5e58ffbd85279cf
Kalindro <25466220+Kalindro@users.noreply.github.com>
2023-03-06 13:06:55 +0100

    Fixes

--- prime_functions/momentums.py
+++ prime_functions/momentums.py
@@ -11,20 +11,6 @@ def momentum_ranking_with_parity(pairs_history_df_list: list[pd.DataFrame], mome
     x = momentum_ranking_for_pairs_histories()
     d = calc_portfolio_parity()
 
-
-def momentum_ranking_for_pairs_histories(pairs_history_df_list: list[pd.DataFrame], momentum_period: int,
-                                         top_decimal: float = None, top_number: int = None):
-    """Calculate momentum ranking for list of history dataframes"""
-    if top_number and top_decimal:
-        raise AssertionError("You can only provide either top decimal or top number")
-
-    logger.info("Calculating momentum ranking for pairs histories")
-    momentum_dict = {}
-    for pair_df in pairs_history_df_list:
-        pair = pair_df["pair"].iloc[-1]
-        pair_df["momentum"] = pair_df["close"].rolling(momentum_period).apply(_calculate_momentum)
-        momentum_dict[pair] = pair_df["momentum"].iloc[-1]
-
     momentum_df = pd.DataFrame.from_dict(momentum_dict, orient="index", columns=["momentum"])
     sorted_momentum = momentum_df.sort_values("momentum", ascending=False)
 
@@ -39,6 +25,24 @@ def momentum_ranking_for_pairs_histories(pairs_history_df_list: list[pd.DataFram
     bottom_coins_history_df_list = [pair_df for pair_df in pairs_history_df_list if
                                     pair_df["pair"].iloc[-1] in bottom_coins]
     return top_coins_history_df_list, bottom_coins_history_df_list
+
+
+def momentum_ranking_for_pairs_histories(pairs_history_df_list: list[pd.DataFrame], momentum_period: int,
+                                         top_decimal: float = None, top_number: int = None):
+    """Calculate momentum ranking for list of history dataframes"""
+    if top_number and top_decimal:
+        raise AssertionError("You can only provide either top decimal or top number")
+
+    logger.info("Calculating momentum ranking for pairs histories")
+    momentum_dict = {}
+    for pair_df in pairs_history_df_list:
+        pair = pair_df["pair"].iloc[-1]
+        pair_df["momentum"] = pair_df["close"].rolling(momentum_period).apply(_calculate_momentum)
+        momentum_dict[pair] = pair_df["momentum"].iloc[-1]
+
+    pairs_history_df_list_momentum = pairs_history_df_list
+
+    return pairs_history_df_list_momentum
 
 
 def _calculate_momentum(price_closes: pd.DataFrame) -> float:
