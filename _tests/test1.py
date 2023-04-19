73ec79fed4f5607bf83dc1ef11801fa3e24e693d
Kalindro <25466220+Kalindro@users.noreply.github.com>
2023-04-19 21:50:45 +0200

    Fixes

--- _tests/test1.py
+++ _tests/test1.py
@@ -1,12 +1,18 @@
 from API.API_exchange_initiator import ExchangeAPISelect
 from exchange.get_history import GetFullHistoryDF
-
+import vectorbtpro as vbt
 API = ExchangeAPISelect().binance_spot_read_only()
 pair = ["JOE/BTC"]
 timeframe = "1d"
-last_n_candles = 50
+start = "01.01.2023"
+end = "01.02.2023"
 
-history = GetFullHistoryDF().get_full_history(pairs_list=pair, number_of_last_candles=last_n_candles,
-                                              timeframe=timeframe, API=API)
+new_start = "01.12.2022"
+new_end = "01.03.2023"
 
-[print(data) for data in list(history.data.values())]
+history = GetFullHistoryDF().get_full_history(pairs_list=pair, start=start, end=end, timeframe=timeframe, API=API)
+
+for data in list(history.data.values()):
+    print(data.head(10))
+    print(data.tail(10))
+
